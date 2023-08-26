9837e977cf560ed5832ace4b6d64e353db6f63c8
Ridho Aji <ridhoaji921@gmail.com>
2023-08-26 13:06:48 +0700

    change triangel

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-function persegi(sisi:number) {
-  return sisi*sisi
+function triangle(yes:number) {
+  return yes ?? 9;
 }
 
-function keliling(sisi:number) {
-  return sisi*4
-}
-
-function segitiga(tinggi:number, alas:number) {
-  return alas*0.5*tinggi
-}
-
-console.log(persegi(4));
-console.log(segitiga(2, 8));
-console.log(segitiga(4, 2));
-console.log(segitiga(5, 3));
+console.log(triangle(10));
